f4b204be483046d2a294ac005a1c854830470769
Nealxero <santixero@gmail.com>
2022-09-09 15:59:34 +0000

    forgot password style done

--- src/front/js/pages/forgot.js
+++ src/front/js/pages/forgot.js
@@ -1,21 +1,21 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/home.css";
+import "../../styles/forgot.css";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export const ForgotPassword = () => {
   return (
-    <div id="card">
+    <div id="card2">
       <div id="card-content">
-        <div id="card-title">
+        <div id="card-title2">
           <h2>Get a new password in one click</h2>
           <div className="underline-title"></div>
         </div>
         <form className="form">
-          <label htmlFor="user-email">Enter the email address linked to your account</label>
+          <label htmlFor="user-email">Forgot your password? Enter the email address linked to your account</label>
           <input
-            id="user-email"
+            id="user-email2"
             className="form-content"
             type="email"
             name="email"
@@ -24,8 +24,8 @@ export const ForgotPassword = () => {
           />
           <div className="form-border"></div>
 
-          <input id="submit-btn" type="submit" name="submit" value="SEND" />
-          <p id="signup">
+          <input id="submit-btn2" type="submit" name="submit" value="SEND" />
+          <p id="signup2">
             <Link to="/signup">Cancel</Link>
           </p>
         </form>
