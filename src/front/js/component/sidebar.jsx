00b25e48e57d3f5649dfb9067a6ee5c1f1d618a1
SimoneHell <simone.hall.speaks@gmail.com>
2022-09-09 13:42:37 +0000

    Imported React correcrtly. Preview now working

--- src/front/js/component/sidebar.jsx
+++ src/front/js/component/sidebar.jsx
@@ -1,62 +1,64 @@
-import React from "react";
-import {
-    FaTh,
-    FaUserAlt,
-    FaHeart,
-    FaSearch,
-    FaBars
-}
-from "react-icons/fa";
+import React, { useState } from "react"; // you were missing the useState
+import { FaTh, FaUserAlt, FaHeart, FaSearch, FaBars } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
-const Sidebar = ({children})=>{
-    const[isOpen ,setIsOpen] = useState(false);
-    const toggle = () => setIsOpen (!isOpen);
-    const menuItem=[
-        {
-            path:"/",
-            name:"dashboard",
-            icon: <FaTh/>
-        },
-        {
-            path:"/account",
-            name:"Account",
-            icon: <FaUserAlt/>
-        },
-        {
-            path:"/recipesearch",
-            name:"Recipe Search",
-            icon: <FaSearch/>
-        },
+const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const menuItem = [
+    {
+      path: "/",
+      name: "dashboard",
+      icon: <FaTh />,
+    },
+    {
+      path: "/account",
+      name: "Account",
+      icon: <FaUserAlt />,
+    },
+    {
+      path: "/recipesearch",
+      name: "Recipe Search",
+      icon: <FaSearch />,
+    },
 
-        {
-            path:"/favorites",
-            name:"Davorites",
-            icon: <FaHeart/>
-        },
-        
-        
-    ]
-    return (
-        <div className="container">
-            <div className="sidebar">
-                <div className="top_section">
-                    <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
-                    <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
-                        <FaBars onClick={toggle}/>
-                    </div>
-                    {
-                        menuItem.map((item, index)=>(
-                            <NavLink to={item.path} key={index} className="link" activeclassName="active">
-                                <div className="icon">{item.icon}</div>
-                                <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>                            </NavLink>
-                        ))
-                    }
-                </div>
-            </div>
-            <main>{children}</main>
+    {
+      path: "/favorites",
+      name: "Davorites",
+      icon: <FaHeart />,
+    },
+  ];
+  return (
+    <div className="container">
+      <div className="sidebar">
+        <div className="top_section">
+          <h1 style={{ display: isOpen ? "block" : "none" }} className="logo">
+            Logo
+          </h1>
+          <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars">
+            <FaBars onClick={toggle} />
+          </div>
+          {menuItem.map((item, index) => (
+            <NavLink
+              to={item.path}
+              key={index}
+              className="link"
+              activeclassName="active"
+            >
+              <div className="icon">{item.icon}</div>
+              <div
+                style={{ display: isOpen ? "block" : "none" }}
+                className="link_text"
+              >
+                {item.name}
+              </div>{" "}
+            </NavLink>
+          ))}
         </div>
-    );
+      </div>
+      <main>{children}</main>
+    </div>
+  );
 };
 
-export default Sidebar
+export default Sidebar;
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,34 +1,34 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './home.css'
+import "../styles/home.css"; // the import here was ./home.css
 import injectContext from "./store/appContext";
 import Dashboard from "./pages/dashboard.jsx";
 import RecipeSearch from "./pages/recipesearch.jsx";
 import Favorites from "./pages/favorites.jsx";
-import Sidebar from "./component/sidebar.jsx";
-
+import Sidebar from "./component/sidebar.jsx"; 
+import Account from "./pages/account.jsx"; // this wasn't here
 
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <Sidebar>
-                    <Routes>
-                        <Route path ="/"element={<Dashboard/>}/>
-                        <Route path ="/dashboard"element={<Dashboard/>}/>
-                        <Route path ="/account"element={<Account/>}/>
-                        <Route path ="/recipesearch"element={<RecipeSearch/>}/>
-                        <Route path ="/favorites"element={<Favorites/>}/>
-                    </Routes>
-                </Sidebar>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/recipesearch" element={<RecipeSearch />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </Sidebar>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
