4e4c50759926b4f4ca6c988d135e0630b37193d8
SimoneHell <simone.hall.speaks@gmail.com>
2022-09-12 05:42:21 +0000

    Created Add Modal and Log out Modal. Some issues with button that opens modal.

--- src/front/js/pages/account.jsx
+++ src/front/js/pages/account.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+import Button from 'react-bootstrap/Button';
+import LogoutModal from '../component/logout-modal.jsx';
 
 const Account = ()=>{
+    const [openLogoutModal, setOpenLogoutModal] = useState(false);
+
     return (
         <div>
         <div className="pages">
             <h2>Account</h2>
         </div>
-        poop poppo poppo
+        
+        <Button variant="primary" onClick={() =>{setOpenLogoutModal(true);}}>Click me</Button>{' '}
+        {openLogoutModal && <LogoutModal/>}
+
+     
         </div>
     );
 };
 
-export default Account
+export default Account;
