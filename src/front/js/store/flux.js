213e4fffad33b1baa78d55b2e1e21d1e6c91f817
Nealxero <santixero@gmail.com>
2022-09-17 12:47:19 +0000

    Flux and Store functions created, starting the account styling and store testing

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,70 +1,84 @@
-const API_URL = "https://3001-nealxero-finalprojectna-fxjpcu5gpuq.ws-eu64.gitpod.io/"
+const API_URL =
+  "https://3001-nealxero-finalprojectna-fxjpcu5gpuq.ws-eu64.gitpod.io/";
 
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			users: [],
-			meals: [],
-			favorites: []
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			
-			loadUsers: (users) => {
-				const store = getStore();
+  return {
+    store: {
+      users: {
+        favorites: [],
 
-				fetch(`${API_URL}users`)
-					.then(data => data.json())
-					.then(async (data) => {
-						let newArray = store.users
-						
-						newArray = newArray.concat(data);
-						setStore({ users: newArray });
-					})
-			},
+        daily_plans: [],
+      },
+      meals: [],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
 
-			loadIndivUser: (url, id, plan) => {
-				const store = getStore();
+      loadUsers: (users) => {
+        const store = getStore();
 
-				fetch(url)
-					.then(data => data.json())
-					.then(data => {
-						let newArray = store[plan]
-						newArray[id].info = data.result
-						setStore({ [plan]: newArray });
+        fetch(`${API_URL}users`)
+          .then((data) => data.json())
+          .then(async (data) => {
+            let newArray = store.users;
 
-					})
-			},
-			
-			updateFavoriteList: (newElement) => {
-				const store = getStore();
-				const newFavorites = [...store.favorites, newElement];
-				setStore({ favorites: newFavorites });
-			  },
-		
-			  deleteFavorite: (data) => {
-				const store = getStore();
-		
-				let newFavorites = store.favorites.filter((item, i) => i != data);
-				setStore({ favorites: newFavorites });
-			  },
+            newArray = newArray.concat(data);
+            setStore({ users: newArray });
+          });
+      },
 
-			},
-			
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-		}
-	};
+      loadMeals: (meals) => {
+        const store = getStore();
 
+        fetch(`${API_URL}meals`)
+          .then((data) => data.json())
+          .then(async (data) => {
+            let newArray = store.meals;
+
+            newArray = newArray.concat(data);
+            setStore({ meals: newArray });
+          });
+      },
+
+      loadIndivUser: (url, id, plans) => {
+        const store = getStore();
+
+        fetch(url)
+          .then((data) => data.json())
+          .then((data) => {
+            let newArray = store[plans];
+            newArray[id].info = data.result;
+            setStore({ [plans]: newArray });
+          });
+      },
+
+      updateFavoriteList: (newElement) => {
+        const store = getStore();
+        const newFavorites = [...store.users.favorites, newElement];
+        setStore({ favorites: newFavorites });
+      },
+
+      deleteFavorite: (data) => {
+        const store = getStore();
+
+        let newFavorites = store.users.favorites.filter((item, i) => i != data);
+        setStore({ favorites: newFavorites });
+      },
+    },
+
+    getMessage: async () => {
+      try {
+        // fetching data from the backend
+        const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+        const data = await resp.json();
+        setStore({ message: data.message });
+        // don't forget to return something, that is how the async resolves
+        return data;
+      } catch (error) {
+        console.log("Error loading message from backend", error);
+      }
+    },
+  };
+};
 
 export default getState;
