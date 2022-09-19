9c3f5ecf77b5c6e5e0746d3cb841f6778eea4d08
Nealxero <santixero@gmail.com>
2022-09-19 16:29:57 +0000

    account settings skeleton

---
+++ src/front/js/component/account-changes.jsx/account-email.jsx
@@ -0,0 +1,49 @@
+import React, { useContext, useState } from "react";
+import { useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "../../styles/account.css";
+import getState from "../store/flux";
+import AddModal from "../component/add-modal.jsx";
+import { Accordion } from "react-bootstrap";
+import AccordionBody from "react-bootstrap/esm/AccordionBody";
+import AccordionItem from "react-bootstrap/esm/AccordionItem";
+import { InputGroup } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+
+function  EmailAccordion ()  {
+
+
+    return ( 
+        <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Change Email</Accordion.Header>
+          <Accordion.Body>
+            <Form.Label htmlFor="basic-url">
+              Please enter your new Email
+            </Form.Label>
+            <InputGroup classname="mb-2">
+              <Form.Control
+                placeholder="Email"
+                aria-label="username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <Form.Label htmlFor="basic-url">
+              Please confirm your new Email
+            </Form.Label>
+            <InputGroup classname="mb-2">
+              <Form.Control
+                placeholder="Email"
+                aria-label="username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+
+    )
+}
+
+export default EmailAccordion
---
+++ src/front/js/component/account-changes.jsx/account-password.jsx
@@ -0,0 +1,48 @@
+import React, { useContext, useState } from "react";
+import { useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "../../styles/account.css";
+import getState from "../store/flux";
+import AddModal from "../component/add-modal.jsx";
+import { Accordion } from "react-bootstrap";
+import AccordionBody from "react-bootstrap/esm/AccordionBody";
+import AccordionItem from "react-bootstrap/esm/AccordionItem";
+import { InputGroup } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+
+ function PasswordAccordion ()  {
+
+    return (
+        <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Change Password</Accordion.Header>
+          <Accordion.Body>
+            <Form.Label htmlFor="basic-url">
+              Please enter your new Password
+            </Form.Label>
+            <InputGroup classname="mb-2">
+              <Form.Control
+                placeholder="Email"
+                aria-label="username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <Form.Label htmlFor="basic-url">
+              Please confirm your new Password
+            </Form.Label>
+            <InputGroup classname="mb-2">
+              <Form.Control
+                placeholder="Email"
+                aria-label="username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      
+    )
+}
+
+export default PasswordAccordion
